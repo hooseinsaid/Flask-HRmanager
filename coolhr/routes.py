c5a2de62f9f46d393de9466615418534c2513a52
Chiemerie Chibueze Ezechukwu <chezzreel@gmail.com>
2020-05-25 15:48:58 +0200

    minor

--- coolhr/routes.py
+++ coolhr/routes.py
@@ -441,7 +441,7 @@ def save_images(form_image, imageto_replace):
     #delete former image fn if it exists in the directory and filename is not None from db
     if imageto_replace is not None:
         if imageto_replace != '':
-            imageto_replace_path = os.path.join(app.root_path, 'static', 'profile_images', imageto_replace)
+            imageto_replace_path = os.path.join(app.root_path, 'static', 'profile_images', 'avatars', imageto_replace)
             if os.path.exists(imageto_replace_path):
                 os.remove(imageto_replace_path)
 
@@ -449,8 +449,8 @@ def save_images(form_image, imageto_replace):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_image.filename)
     image_fn = random_hex + f_ext
-    image_path = os.path.join(app.root_path, 'static', 'profile_images', image_fn)
-    output_size = (400, 400)
+    image_path = os.path.join(app.root_path, 'static', 'profile_images', 'avatars', image_fn)
+    output_size = (600, 600)
     i = Image.open(form_image)
     i.thumbnail(output_size)
     i.save(image_path)
